fb74aa73fd3e0fe7eaf077637bf6a321bec9b147
Ismael Banderas Barrera <banderasismael@gmail.com>
2025-02-04 21:29:21 -0600

    Añadi ejercicio 2 al index.php

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>Práctica 3</title>
+    <title>Práctica 4</title>
 </head>
 <body>
     <h2>Ejercicio 1</h2>
@@ -29,6 +29,32 @@
         echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
         echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
         echo '</ul>';
+
+        /*--------------------------------------------------------------------------------*/
+        // EJERCICIO 2
+        echo "<h2>Ejercicio 2</h2>";
+        $a = "ManejadorSQL";
+        $b = 'MySQL';
+        $c = &$a;
+
+        echo "<p>Muestra del contenido de cada variable</p>";
+        echo "\$a = $a <br>";
+        echo "\$b = $b <br>";
+        echo "\$c = $c <br>";
+
+        /// Modificacion de las variables originales (punto b)
+        $a = "PHP server";
+        $b = &$a;
+        
+        echo "<p>Muestra del contenido de cada variable con las asignaciones extra</p>";
+        echo "\$a = $a <br>";
+        echo "\$b = $b <br>";
+        echo "\$c = $c <br>";
+
+        echo '<h4>Explicación:</h4>';
+        echo "<p>Cuando modificamos el valor de \$a, indirectamente modificamos también el valor de \$c, ya que esta variable contiene la referencia de a. Esto mismo sucede con la variable \$b. Entonces como las variables \$b y \$c son referencias de a, terminan mostrando el mismo contenido que \$a</p>";
+
+
     ?>
 </body>
 </html>
