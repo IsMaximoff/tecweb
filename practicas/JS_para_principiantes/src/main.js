18721086557b5761619e21f237f3a4cfef3516fd
Ismael Banderas Barrera <banderasismael@gmail.com>
2025-02-23 23:48:38 -0600

    Agregué ejercicio 18

--- practicas/JS_para_principiantes/src/main.js
+++ practicas/JS_para_principiantes/src/main.js
@@ -249,6 +249,23 @@ function ejemplo_17() {
     document.getElementById("ejemplo_17").innerHTML = resultado;
 }
 
+function ejemplo_18() {
+    function convertirCastellano(x) {
+        switch (x) {
+            case 1: return "uno";
+            case 2: return "dos";
+            case 3: return "tres";
+            case 4: return "cuatro";
+            case 5: return "cinco";
+            default: return "valor incorrecto";
+        }
+    }
+
+    var valor = prompt("Ingresa un valor entre 1 y 5", "");
+    valor = parseInt(valor);
+    document.getElementById("ejemplo_18").innerHTML = "<h3>" + convertirCastellano(valor) + "</h3>";
+}
+
 
 /*function getDatos()
 {
